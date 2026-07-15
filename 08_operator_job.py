d28d676a4904953b22e0f581dc5bed7818939cbc
Hwany998 <wpghks1991@gmail.com>
2026-07-15 18:07:39 +0900

    복합연산자 실습

---
+++ 08_operator_job.py
@@ -0,0 +1,19 @@
+#실습1
+a = 17
+b = 5
+print(a + b)    # 22
+print(a - b)    # 12
+print(a * b)    # 85
+print(a / b)    # 3.4 (소수점까지)
+print(a // b)   # 3 (몫)
+print(a % b)    # 2 (나머지)
+print(a ** b)   # 1419857 (거듭제곱)
+
+#실습2
+a = 22
+b = 20
+c = 24
+print((a + b + c)/3)# 22.0 (평균, 소수점 주의)
+line = 8            # 변 길이
+print(line ** 2)    # 64 (정사각형 넓이)
+print(a * b * c)    # 10560 (직육면체 부피)
