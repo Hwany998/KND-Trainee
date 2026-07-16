68e14a76484f3f5611be13fb0b24189b5981051a
Hwany998 <wpghks1991@gmail.com>
2026-07-16 14:28:35 +0900

    9번 파일 코드 수정 및 실습6까지 추가

--- 10_input&typech_job.py
+++ 10_input&typech_job.py
@@ -20,3 +20,14 @@ print("나눗셈: "+str(round(a / b, 2)))
 print("값: "+str(a // b))
 print("나머지: "+str(a % b))
 print("거듭제곱: "+str(a ** b))
+
+#실습5
+temp = int(input("현재 온도: "))
+print("출력 결과 1: "+ str(temp > 80))
+print("출력 결과 2: "+ str(temp >= 0))
+
+#실습6
+math = int(input("수학점수: "))
+eng = int(input("영어점수: "))
+science = int(input("과학점수: "))
+print("평균 60점을 넘었는가? "+ str(((math+eng+science)/3) >= 60))
