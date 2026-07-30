f7f3a2e3ffd95cb98cfa6ad2f07b6a2d02bcd1d0
Hwany998 <wpghks1991@gmail.com>
2026-07-30 14:32:30 +0900

    조건반복응용, list관련 추가교육

--- 16_for.py
+++ 16_for.py
@@ -124,3 +124,115 @@ for dan2 in range(1, 10):       #단수 1~9
         for star2 in range(1,10):   #조건 기준으로 1~9까지 곱
             print(f"{dan2} x {star2} = {dan2 * star2}")
         print(f"{dan2}단 끝")   #1~9까지 곱한 후 해당 단수 끝
+
+#무한루프 유의
+count = 1
+while count <= 3:
+    print(count)
+    count += 1  #얘 없으면 무한루프 / 멈출 땐 ctrl c / command c
+
+goal = 7
+start_num = int(input("제가 생각하는 중인 값을 1-10까지 찾아보세요:"))
+while start_num != goal:
+    if(start_num == goal):
+        print("정답입니다.")
+    else:
+        print("틀렸습니다. 다시 입력하세요.")
+        start_num = int(input())
+print("종료")
+
+#break 사용시 반복 바로 탈출
+# input_sum = 0
+# while True: #조건만 보면 무한반복하는 코드
+#     input("값을 입력하세요. 값의 누적이 15 넘으면 종료")
+#     input_sum += user_input
+
+#     if(input_sum > 15):
+#         print("누적합계:", input_sum, "입력 종료")
+#         break
+# print("break를 통해 while문 나가면 코드 실행")
+
+#사용자 입력값을 확인만 하고 저장할 필요 없다면!
+while True:
+    x = input("입력 (종료는 q를 입력하세요): ")
+    if x == 'q':
+        break
+    print("입력받은 값:", x)
+
+# n = int(input("횟수: "))
+# for i in range(n):
+#     v = int(input("측정값: "))
+#     if v > 80:
+#         print("이상 발생")
+#         print("가동 횟수:", n)
+#         break
+#     else:
+#         print("정상 상태")
+    
+#실습 up down 게임
+#1~50 중 하나의 숫자를 정답 저장
+#사용자 입력값이 정답이 up down인지 출력
+#정답이 나오면 정답, 게임 종료되었다 출력
+c = 22
+m = int(input("1-50 중 하나를 골라:"))
+for i in range (1,50):
+    if(m < c):
+        print("up")
+        m = int(input("재입력:"))
+    elif(m > c):
+        print("down")
+        m = int(input("재입력:"))
+    else:
+        print("정답")
+        break
+print("종료")
+
+#최댓값 찾기
+print("====최댓값====")
+first = int(input("1번째 입력값: "))
+max_value = first
+for i in range(4):
+    v = int(input(f"{i+2}번째 입력: "))
+    if v > max_value:
+        max_value = v
+print("최댓값:", max_value)
+
+    #max_value에는 현 시점 최댓값
+    #v에는 방금 사용자가 입력한 값
+
+#실습: 플래그로 조건 만족값 검색
+print("===플래그실습===")
+check = False
+first_check = int(input("1번째 입력: "))
+max_value2 = first_check
+for i in range(4):
+    a = int(input(f"{i+2}번째 입력: "))
+    if(a > 80):
+        check = True
+        print("발견")
+        break
+
+#조건 맞는 값 출력
+temps2 = [23, 32, 30, 22, 29, 35]
+for i in temps2:
+    if i >= 30:
+        print(f"고온: {i}")
+
+#and로 두 조건 만족 값 출력
+times = [2, 5, 6, 8, 10, 3, 4]
+for t in times:
+    if (t >= 5 and t <= 10):
+        print(t)
+
+#실습3 조건 맞는 값만 골라 평균 구하기
+temps3 = [32, 29, 31, 36, 20, 28]
+memory_temps3 = []
+temps_sum = 0
+temps_count = 0
+for v in temps3:
+    if(v > 30):
+        memory_temps3.append(v)
+        temps_sum += v
+        temps_count += 1
+print(f"({memory_temps3[:]} -> 합{temps_sum}, 개수{temps_count})")
+print(f"고온평균: {temps_sum/3}")
