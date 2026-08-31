841f8a392455ea1222edab1afedafeaf1d381878
Hwany998 <wpghks1991@gmail.com>
2026-08-31 15:22:09 +0900

    상하공정 분류 및 필요 값 출력과정

--- jupyter/jupyter_01.py
+++ jupyter/jupyter_01.py
@@ -29,13 +29,13 @@ print(PROCESS_KR.get('BOF'))                # 전로 (안전하게 get문)
 print(PROCESS_KR.get('BOF1', '미등록'))       # 태그 없는 것이기 때문에 미등록 출력
 
 MEASURE_KR = {
-    'VIB' : '진동',
-    'CUR' : '전류',
-    'SPD' : '온도',
-    'PRS' : '압력',
-    'TMP' : '유량',
-    'FLW' : '속도',
-    'TMP' : '레벨'
+    "VIB":"진동",
+    "CUR":"전류",
+    "TMP":"온도",
+    "PRS":"압력",
+    "FLW":"유량",
+    "SPD":"속도",
+    "LVL":"레벨"
 }
 
 print(MEASURE_KR.get('PRS', '미등록'))
@@ -47,7 +47,7 @@ print(df.shape)
 print(df.columns.tolist())
 
 # 공정별 몇 개의 태그가 있는지 세어보기
-split_cols = df['tag'].str.split("-", expand = True)
+split_cols = df['tag'].str.split("-", expand = True)    #표로 만드는 기법
 df["plant"]=split_cols[0]
 df["process"]=split_cols[1]
 df["equip"]=split_cols[2]
@@ -72,3 +72,36 @@ print(df.groupby('process_kr').size())
 코크스     2
 dtype: int64
 '''
+
+#실습. 설비 태그에서 공정 위치 추론
+#24개 태그 읽고 공정과 상하공정 구분을 판정한 표 작성
+#공정별 묶어 태그가 가장 많은 공정 확인
+#계측 항목별 묶어 가장 많은 물리량 확인
+#자동 분류 방식
+#공정 딕셔너리 생성
+PROCESS_DIC = {
+    'SNT' : '소결',
+    'CKO' : '코크스',
+    'BF' : '고로',
+    'BOF' : '전로',
+    'CCM' : '연주',
+    'HSM' : '열간압연',
+    'CRM' : '냉간압연',
+    'UTL' : '유틸리티',
+}
+df['process_kr']=df['process'].map(PROCESS_KR)
+df['measure_kr']=df['measure'].map(MEASURE_KR)
+
+UP_PROCESS = {"SNT", "CKO", "BF", "BOF", "CCM"}
+DOWN_PROCESS = {"HSM", "CRM"}
+
+def updown_sep(process):
+    if process in UP_PROCESS:
+        return "상공정"
+    elif process in DOWN_PROCESS:
+        return "하공정"
+
+df["stage"] = df["process"].map(PROCESS_KR).fillna("미등록")
+
+measure_a=df.groupby("measure_kr").size().sort_values(ascending=False, kind = "stable")
+print(measure_a.idxmax())   #실습
